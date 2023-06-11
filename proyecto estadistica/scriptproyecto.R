29f962205ce22b620be7e9ed7db47bf3a2cfdf91
OviedoSebastian <114544540+OviedoSebastian@users.noreply.github.com>
2023-06-11 17:33:36 -0500

    Update scriptproyecto.R

--- proyecto estadistica/scriptproyecto.R
+++ proyecto estadistica/scriptproyecto.R
@@ -1,6 +1,36 @@
 # cargar paquete readr
 library(readr)
+library(dplyr)
 
 # importar WHO.csv
 WHO <- read_csv("WHO.csv")
+
+#vistas de las tablas
 View(WHO)
+View(analizar)
+View(media_muertes.infantesXpais)
+View(muertes.infantesXpais)
+
+#se sacan las variables a trabajar de la base de datos
+analizar <- select(WHO, 
+                   Country, 
+                   Cod, 
+                   Year,
+                   Status, 
+                   Thinness.10.19.years,
+                   Thinness.5.9.years,
+                   BMI,
+                   Life.expectancy,
+                   Infant.deaths
+)
+
+#analisis sobre las muertes de infantes
+muertes.infantesXpais <- select(WHO, 
+                                Cod, 
+                                Year,
+                                Infant.deaths
+                                 )
+
+media_muertes.infantesXpais <- muertes.infantesXpais %>%
+  group_by(Cod, Year) %>%
+  summarize(mean_ht = mean(Infant.deaths, na.rm = TRUE) )
