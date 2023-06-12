627127fe6cff6fc8e6f5bb62e5955407ba2ba9cb
OviedoSebastian <114544540+OviedoSebastian@users.noreply.github.com>
2023-06-12 16:55:43 -0500

    Update scriptproyecto.R

--- proyecto estadistica/scriptproyecto.R
+++ proyecto estadistica/scriptproyecto.R
@@ -18,30 +18,62 @@ analizar <- select(WHO,
                    Country, 
                    Cod, 
                    Year,
-                   Status, 
-                   Thinness.10.19.years,
-                   Thinness.5.9.years,
-                   BMI,
+                   Status,
                    Life.expectancy,
                    Infant.deaths
 )
 
-#listado de paises sin repeticiones
-listadoDePaises <- distinct(WHO, Cod )
-listadoDeanios <- distinct(WHO, Year )
 
 #analisis sobre las muertes de infantes
-muertes.infantes <- select(WHO, 
-                            Cod, 
-                            Year,
-                            Infant.deaths
-                            )
+muertes.infantes <- select(WHO,
+                           Country,
+                           Cod,
+                           Percentage.expenditure,
+                           Year,
+                           Infant.deaths
+)
 
-muertes.infantesXpais_2010 <- muertes.infantes %>%
-  filter(Year == "2010") %>%
+
+#listado de paises sin repeticiones
+listadoDePaises <- distinct(WHO, Country )
+listadoDeanios <- distinct(WHO, Year )
+
+years <- c("2014", "2015", "2016")
+paisesAmericaNorte <- c("CAN", "USA", "MEX")
+paisesAmericaCentral <- c("BLZ", "CRI", "GTM", "HND", "NIC", "PAN", "SLV")
+Caribe <- c( "ATG", "BHS", "BRB", "CUB", "DOM", "GRD", "HTI", "JAM", "LCA", "VCT", "TTO")
+paisesAméricadelSur <- c("VEN", "URY", "PER",  "PRY", "GUY", "ECU", "COL", "CHL", "BRA", "BOL", "ARG")
+
+muertes.infantesXAmericaNorte <- muertes.infantes %>%
+  filter( Year %in% years & Cod %in%  paisesAmericaNorte) %>%
   group_by(Cod) 
 
+muertes.infantesXAmericaCentral <- muertes.infantes %>%
+  filter( Year %in% years & Cod %in%  paisesAmericaCentral) %>%
+  group_by(Cod)
 
+muertes.infantesXCaribe <- muertes.infantes %>%
+  filter( Year %in% years & Cod %in%  Caribe) %>%
+  group_by(Cod)
+
+muertes.infantesXAméricadelSur <- muertes.infantes %>%
+  filter( Year == '2014' & Cod %in%  paisesAméricadelSur) %>%
+  group_by(Cod)
+
+View(muertes.infantesXAmericaNorte)
+View(muertes.infantesXAmericaCentral)
+View(muertes.infantesXCaribe)
+View(muertes.infantesXAméricadelSur)
+
+hist(muertes.infantesXAméricadelSur$Year, 
+     breaks = 10, main = "Numero de lactantes muerto por anio", 
+     xlab = "Valores")
+
+
+#fin del analisis momentaneo -- aqui vamos, ignoren lo de abajo
+
+
+#otra cosa
 listadomuertesXanioYpais <- list()  # Lista para almacenar los resultados
 
 for ( i in 1:nrow(listadoDeanios) ) {
@@ -59,3 +91,5 @@ for ( i in 1:nrow(listadoDeanios) ) {
 for (i in 1:length(listadomuertesXanioYpais) ) {
   View( listadomuertesXanioYpais[[i]] )
 }
+
+
