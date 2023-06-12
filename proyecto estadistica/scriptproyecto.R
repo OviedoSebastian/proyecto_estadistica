2c0027e8858360c369576c1e785f8713b23c1f78
OviedoSebastian <114544540+OviedoSebastian@users.noreply.github.com>
2023-06-11 21:06:48 -0500

    Update scriptproyecto.R

--- proyecto estadistica/scriptproyecto.R
+++ proyecto estadistica/scriptproyecto.R
@@ -8,8 +8,10 @@ WHO <- read_csv("WHO.csv")
 #vistas de las tablas
 View(WHO)
 View(analizar)
-View(media_muertes.infantesXpais)
-View(muertes.infantesXpais)
+View(muertes.infantesXpais_2011)
+View(muertes.infantes)
+View(listadoDePaises)
+View(listadoDeanios)
 
 #se sacan las variables a trabajar de la base de datos
 analizar <- select(WHO, 
@@ -24,13 +26,36 @@ analizar <- select(WHO,
                    Infant.deaths
 )
 
-#analisis sobre las muertes de infantes
-muertes.infantesXpais <- select(WHO, 
-                                Cod, 
-                                Year,
-                                Infant.deaths
-                                 )
+#listado de paises sin repeticiones
+listadoDePaises <- distinct(WHO, Cod )
+listadoDeanios <- distinct(WHO, Year )
 
-media_muertes.infantesXpais <- muertes.infantesXpais %>%
-  group_by(Cod, Year) %>%
-  summarize(mean_ht = mean(Infant.deaths, na.rm = TRUE) )
+#analisis sobre las muertes de infantes
+muertes.infantes <- select(WHO, 
+                            Cod, 
+                            Year,
+                            Infant.deaths
+                            )
+
+muertes.infantesXpais_2010 <- muertes.infantes %>%
+  filter(Year == "2010") %>%
+  group_by(Cod) 
+
+
+listadomuertesXanioYpais <- list()  # Lista para almacenar los resultados
+
+for ( i in 1:nrow(listadoDeanios) ) {
+
+  temporalnumber <- as.character(listadoDeanios[i,])
+  
+  temporaldata <- muertes.infantes %>%
+  filter(Year == temporalnumber) %>%
+  group_by(Cod)
+  
+  listadomuertesXanioYpais[[i]] <- temporaldata  # Guardar los resultados en la lista
+}
+
+# Visualizar cada elemento de la lista en vistas separadas
+for (i in 1:length(listadomuertesXanioYpais) ) {
+  View( listadomuertesXanioYpais[[i]] )
+}
